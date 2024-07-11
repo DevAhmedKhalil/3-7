14146c962614c40915ee5cc2e2be945b63d5a0bc
DevAhmedKhalil <dev.akhalil01@gmail.com>
2024-07-11 14:46:31 +0300

    Get all categories, And adding pagenation.

--- services/categoryService.js
+++ services/categoryService.js
@@ -3,12 +3,22 @@ const CategoryModel = require("../models/categoryModel");
 const slugify = require("slugify");
 const asyncHandler = require("express-async-handler");
 
-exports.getCategories = (req, res) => {
-  // const name = req.body.name;
-  // console.log(req.body);
-  res.send();
-};
+// @desc      Get a list of categories
+// @route     GET /api/v1/categories
+// @access    Public 'anyone'
+exports.getCategories = asyncHandler(async (req, res) => {
+  const page = parseInt(req.query.page) || 1;
+  const limit = parseInt(req.query.limit) || 2;
+  const skip = (page - 1) * limit;
 
+  const categories = await CategoryModel.find({}).skip(skip).limit(limit);
+
+  res.status(200).json({ results: categories.length, page, data: categories });
+});
+
+// @desc      Create a new category
+// @route     POST /api/v1/category
+// @access    Private 'admin'
 exports.createCategory = asyncHandler(async (req, res) => {
   const { name } = req.body;
   console.log("name: " + name);
