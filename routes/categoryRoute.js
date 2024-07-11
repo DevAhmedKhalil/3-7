bd93ee2caa4d09c62ed6a6e217930708cfe191f4
Ahmed Khalil <109757454+DevAhmedKhalil@users.noreply.github.com>
2024-07-11 19:56:42 +0300

    Update categoryRoute.js

--- routes/categoryRoute.js
+++ routes/categoryRoute.js
@@ -1,8 +1,8 @@
 const express = require("express");
-const { getCategories } = require("../services/categoryService");
+const { createCategore } = require("../services/categoryService");
 const router = express.Router();
 
 //! Routes
-router.get("/", getCategories);
+router.get("/", createCategore);
 
 module.exports = router;
