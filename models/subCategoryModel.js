4d2cc243bcbd5d158ebb98464a8eaf988329330a
DevAhmedKhalil <dev.akhalil01@gmail.com>
2024-07-13 12:49:47 +0300

    Adding subCategoryModel.

---
+++ models/subCategoryModel.js
@@ -0,0 +1,29 @@
+const mongoose = require("mongoose");
+
+//! 1. Create schema
+const subCategorySchema = new mongoose.Schema(
+  {
+    name: {
+      type: "string",
+      required: [true, "Subcategory name is required."],
+      unique: [true, "Subcategory must be unique."],
+      minlength: [3, "Too short subcategory name."],
+      maxlength: [32, "Too long subcategory name."],
+    },
+    slug: {
+      type: "string",
+      lowercase: true,
+    },
+    category: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Category",
+      required: [true, "Subcategory must be refered to parent Category."],
+    },
+  },
+  { timestamps: true }
+);
+
+//! 2. Create model
+const subCategoryModel = mongoose.model("subCategory", subCategorySchema);
+
+module.exports = subCategoryModel;
