7b7bb3e9d868b0f6b14b5bad2e86fdce33278832
DevAhmedKhalil <dev.akhalil01@gmail.com>
2024-11-23 07:28:21 +0200

    Add Token Validation Logic.

--- services/authService.js
+++ services/authService.js
@@ -13,8 +13,22 @@ exports.signup = asyncHandler(async (req, res, next) => {
   });
 
   // 2- Generate token
-  const token = jwt.sign({ userId: User._id }, process.env.JWT_SECRET_KEY, {
-    expiresIn: process.env.JWT_EXPIRE_TIME,
+  const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET_KEY, {
+    algorithm: "HS256",
+    expiresIn: process.env.JWT_EXPIRE_TIME || "7d",
+  });
+
+  // 3- Token Validation Logic
+  // console.log("User ID:", user._id);
+  // console.log(process.env.JWT_SECRET_KEY);
+  // console.log("Generated Token:", token);
+
+  jwt.verify(token, process.env.JWT_SECRET_KEY, (err, decoded) => {
+    if (err) {
+      console.error("JWT Validation Error:", err.message);
+    } else {
+      console.log("Decoded Token:", decoded);
+    }
   });
 
   res.status(201).json({
@@ -22,3 +36,5 @@ exports.signup = asyncHandler(async (req, res, next) => {
     token,
   });
 });
+
+// exports.login = asyncHandler(async (req, res, next) => {});
